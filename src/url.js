3457007597efbad66eb82473c39a2d27c15e59e3
Virola <virola.zhu@gmail.com>
2015-01-28 10:11:24 +0800

    update

--- src/url.js
+++ src/url.js
@@ -18,7 +18,20 @@ define(
                 POST_PROGRAM: 'act/success'
             },
 
-            'dep': {
+            'asset': {
+                USER_LOGIN: '#/',
+
+                GET_ACCOUNT: 'user/info',
+                POST_SIGN: 'user/sign',
+                GET_ACT_STATUS: 'act/status',
+
+                POST_BOUNCE: 'act/bounce',
+                POST_SHAKE_GIFT: 'act/shake',
+                POST_MESSAGE: 'act/success',
+
+                GET_PROGRAM_LIST: 'act/program',
+                GET_PROGRAM_RANK: 'act/rank',
+                POST_PROGRAM: 'act/success'
             }
         };
 
